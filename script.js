eb429ada34a9827e23be2e0bd4fef5ff1e6614fd
rafa <rafumojica1@gmail.com>
2022-09-19 17:20:00 +0800

    initialized new repository

---
+++ script.js
@@ -0,0 +1,21 @@
+const browse = document.getElementById("browse");
+
+browse.addEventListener("mouseover", (event) => {
+        let video = document.querySelector(".landing__video")
+        let muted = video.setAttribute('muted')
+        return muted
+}
+
+// let isMouseHover = false
+// let test = document.getElementById("browse");
+
+// test.addEventListener("mouseleave", function (event) {
+//   isMouseHover = false
+//   event.target.volume = 0.5
+//   console.log(isMouseHover)
+// }, false);
+// test.addEventListener("mouseover", function (event) {
+//   isMouseHover = true
+//   event.target.volume = 0
+//   console.log(isMouseHover)
+// }, false);
