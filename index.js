54336fc9f6198e317b468e6ee91aad7348bab9ce
rafa <rafumojica1@gmail.com>
2022-09-21 15:33:54 +0800

    fetched films

---
+++ index.js
@@ -0,0 +1,31 @@
+// API 1: https://ghibliapi.herokuapp.com/films
+
+const filmsDataEl = document.querySelector(".movies__wrapper");
+
+async function main() {
+  const films = await fetch("https://ghibliapi.herokuapp.com/films");
+  const filmsData = await films.json();
+  filmsDataEl.innerHTML = filmsData.map((films) => filmsHTML(films)).join("");
+  console.log(filmsData);
+}
+
+main();
+
+function showMovie(id) {
+    localStorage.setItem("id", id)
+  window.location.href = `${window.location.origin}/movie.html`
+}
+
+function filmsHTML(films) {
+  return `<div class="movie" onclick="showMovie(${films.id})">
+    <figure class="movie__img--wrapper">
+      <img
+        src="${films.image}"
+        alt=""
+        class="movie__img"
+      />
+    </figure>
+    <p class="movie__title">${films.title}</p>
+    <p class="movie__year">${films.release_date}</p>
+  </div>`;
+}
